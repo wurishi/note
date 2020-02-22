904416aa32274a258f3c86ea10b6f2a139f0a671
XXL <xurishi@gmail.com>
2020-02-22 20:28:52 +0800

    unity1: 构造分形

---
+++ unity1/Assets/chapter1/Fractal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
