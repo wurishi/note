cdc9fb833e3fa49b535180df0a6d79c1c0295455
XXL <xurishi@gmail.com>
2020-07-20 16:13:57 +0800

    threejs: course 7

---
+++ threejs/course/7.js
@@ -0,0 +1,54 @@
+// import THREE from 'three';
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 沿着x轴方向绘制10个立方体
+!function () {
+    const box = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+    for (let i = 0; i < 10; i++) {
+        const mesh = new THREE.Mesh(box, material);
+        mesh.position.set(i * 15, 0, 0);
+        scene.add(mesh);
+    }
+}();
+
+// 整体平移立方体
+!function () {
+    const box = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+    const object = new THREE.Object3D(); // 创建Object3D对象作为组
+    for (let i = 0; i < 10; i++) {
+        const mesh = new THREE.Mesh(box, material);
+        mesh.position.set(i * 15, 0, 0);
+        object.add(mesh);
+    }
+    object.translateY(100); // 平移所有网格模型
+    scene.add(object);
+}();
+
+// 光源设置
+const point = new THREE.PointLight(0xffffff);// 点光源
+point.position.set(400, 200, 300); // 点光源位置
+scene.add(point); // 点光源添加到场景中
+
+// 相机设置
+const width = window.innerWidth;
+const height = window.innerHeight;
+const k = width / height; // 窗口宽高比
+const s = 100; // 三维场景缩放系数
+const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000); // 创建相机对象
+camera.position.set(200, 300, 200); // 设置相机位置
+camera.lookAt(scene.position); // 设置相机方向(指向场景)
+
+// 创建渲染器对象
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+renderer.setClearColor(0xb9d3ff, 1); // 背景颜色
+document.body.appendChild(renderer.domElement); // 往body插入canvas对象
+// 执行渲染
+renderer.render(scene, camera);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement); // 创建操作控件
+controls.addEventListener('change', () => renderer.render(scene, camera));
