e9ff89ea5f31e924a946fa204d9cf32aaba080db
XXL <xurishi@gmail.com>
2020-08-04 13:19:30 +0800

    tensorflowjs: demo-bodypix

--- tensorflowjs/demos/bodypix/index.js
+++ tensorflowjs/demos/bodypix/index.js
@@ -1,8 +1,9 @@
 import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
+import * as posenet from "@tensorflow-models/posenet";
 
-let width = 200;
-let height = 200;
+let width = 400;
+let height = 400;
 let imgLoaded = false;
 let canvas = null;
 
@@ -93,13 +94,23 @@ loadAndPredict();
 
 async function loadAndPredict(img) {
   imgLoaded = false;
+  // default
   // const net = await bodyPix.load();
+
+  // MobileNetV1
   const net = await bodyPix.load({
     architecture: "MobileNetV1",
     outputStride: 16,
-    multiplier: 0.5,
+    multiplier: 0.75,
     quantBytes: 2,
   });
+
+  // ResNet50
+  // const net = await bodyPix.load({
+  //   architecture: "ResNet50",
+  //   outputStride: 32,
+  //   quantBytes: 2,
+  // });
 
   if (!canvas) {
     canvas = document.createElement("canvas");
@@ -153,7 +164,12 @@ async function bodyPixFn(net, img) {
   if (maskMode == 1) {
     maskImage = bodyPix.toColoredPartMask(segmentation);
   } else {
-    maskImage = bodyPix.toMask(segmentation);
+    maskImage = bodyPix.toMask(
+      segmentation,
+      { r: 0, g: 0, b: 0, a: 255 },
+      { r: 0, g: 0, b: 0, a: 0 },
+      true
+    );
   }
 
   if (drawMode == 0) {
@@ -195,4 +211,77 @@ async function bodyPixFn(net, img) {
       flipHorizontal
     );
   }
+  drawPoses(segmentation, flipHorizontal, canvas.getContext("2d"));
+}
+
+function drawPoses(segmentation, flipHorizontal, ctx) {
+  if (Array.isArray(segmentation)) {
+    segmentation.forEach((s) => {
+      let pose = s.pose;
+      if (flipHorizontal) {
+        pose = bodyPix.flipPoseHorizontal(pose, s.width);
+      }
+      drawKeypoints(pose.keypoints, 0.1, ctx);
+      drawSkeleton(pose.keypoints, 0.1, ctx);
+    });
+  } else {
+    segmentation.allPoses.forEach((pose) => {
+      if (flipHorizontal) {
+        pose = bodyPix.flipPoseHorizontal(pose, segmentation.width);
+      }
+      drawKeypoints(pose.keypoints, 0.1, ctx);
+      drawSkeleton(pose.keypoints, 0.1, ctx);
+    });
+  }
+}
+
+const COLOR = "aqua";
+const LINE_WIDTH = 2;
+
+function drawPoint(ctx, x, y, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = LINE_WIDTH;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0, len = keypoints.length; i < len; i++) {
+    const keypoint = keypoints[i];
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+    const { y, x } = keypoint.position;
+    drawPoint(ctx, x * scale, y * scale, 3, COLOR);
+  }
+}
+
+function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints,
+    minConfidence
+  );
+
+  function toTuple({ y, x }) {
+    return [y, x];
+  }
+
+  adjacentKeyPoints.forEach((p) => {
+    drawSegment(
+      toTuple(p[0].position),
+      toTuple(p[1].position),
+      COLOR,
+      scale,
+      ctx
+    );
+  });
 }
